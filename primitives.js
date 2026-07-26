1a779a9a8d9818aa8c70fc33193c7223996e27d8
roshan-chauhan <imroshann67@gmail.com>
2026-07-26 16:02:22 +0000

    add primitives.js file

---
+++ primitives.js
@@ -0,0 +1,28 @@
+// Question 6
+
+// Ek college admission system.
+
+// Student Name
+
+// Course
+
+// Fees
+
+// Admission Confirmed
+
+// Roll Number
+
+// Timeline follow karo.
+let name="Roshan";
+const course="BSC IT";
+let fees=54625;
+let isConfirmed=true;
+console.log("Name of student :",name);
+console.log("Fees :",fees);
+console.log("Admission :",isConfirmed);
+// After Admission
+let rollNo=7;
+let followTime=true;
+console.log("Roll No :",rollNo);
+console.log("Following time :",followTime);
+
