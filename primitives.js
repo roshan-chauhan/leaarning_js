a9febc662d9e9101216c100eac77d20b0d0f4cb2
roshan-chauhan <imroshann67@gmail.com>
2026-07-26 16:48:38 +0000

    again primitve

--- primitives.js
+++ primitives.js
@@ -25,5 +25,61 @@ let rollNo=7;
 let followTime=true;
 console.log("Roll No :",rollNo);
 console.log("Following time :",followTime);
-console.log("=============")
+console.log("=============");
+
+// Question 7
+
+// Ek bank account system.
+
+// Account Holder
+
+// Balance
+
+// KYC Status
+
+// Account Number
+
+// Timeline follow karo.
+let accHolder="Roshan";
+let balance=49000;
+let isKYC=true;
+let accNumber=108297293482436;
+console.log("AccHolder Name :",accHolder);
+console.log("Balance left :",balance);
+console.log("KYC confirmed :",isKYC);
+console.log("Account Number :",accNumber);
+console.log("------------------------")
+
+// Story
+// Ek user Netflix account banata hai.
+
+// Initially account create hota hai.
+// Email verify nahi hui.
+// Subscription plan choose hota hai.
+// Payment successful hota hai.
+// Membership ID baad me assign hoti hai.
+// User subscription cancel karta hai.
+// Membership ID intentionally remove hoti hai.
+// Requirements
+// Sab phases print karo.
+// Har important variable ka typeof bhi print karo.
+// undefined aur null dono ka proper use karo.
+let isCreated=true;
+let emailVerified=false;
+let subscription=true;
+let isSuccessful=true;
+let memberID=undefined;
+console.log("Account Created :",isCreated);
+console.log("Email Verified :",emailVerified);
+console.log("subscription :",subscription);
+console.log("isSuccessful :",isSuccessful);
+
+// After subscription cancellation.
+subscription=false;
+memberID=null;
+console.log(""subscription);
+console.log(memberID)
+
+
+
 
